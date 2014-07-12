1723d193861270d22cf31b6a9afbde2346380a11
Zack Fisch <zack@zfisch.com>
2014-07-12 15:35:55 -0400

    Displaying movies

--- flicks.rb
+++ flicks.rb
@@ -1,9 +1,9 @@
 class Movie
+
 	def initialize(title, rank=0)
 		@title = title
 		@rank = rank
 	end
-
 
 	def to_s
 		"#{@title} has a rank of #{@rank}"
@@ -19,12 +19,16 @@ class Movie
 end
 
 movie1 = Movie.new('Ghostbusters', 10)
-movie1.thumbs_up	
-puts movie1
+
 
 movie2 = Movie.new('Back to the Future', 8)
-movie2.thumbs_down
-puts movie2
+
 
 movie3 = Movie.new('Goldfinger')
-puts movie3
+
+
+movies = [movie1, movie2, movie3]
+movies.each do |movie| 
+	movie.thumbs_up
+	puts movie
+end
