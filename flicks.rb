9363cfacddab963948cfbb75c84ad3c316eafed5
Zack Fisch <zack@zfisch.com>
2014-07-12 22:54:37 -0400

    Adding playlist class

--- flicks.rb
+++ flicks.rb
@@ -18,17 +18,45 @@ class Movie
 	end
 end
 
+class Playlist
+	def initialize(name)
+		@name = name
+		@movies = []
+	end
+
+	def add_movie(movie)
+		@movies << movie
+	end
+
+	def play 
+		puts "#{@name}'s playlist:"
+
+		@movies.each do |movie|
+			movie.thumbs_up
+			puts movie
+		end
+	end
+end
+
+
 movie1 = Movie.new('Ghostbusters', 10)
-
-
 movie2 = Movie.new('Back to the Future', 8)
-
-
 movie3 = Movie.new('Goldfinger')
 
+playlist1 = Playlist.new('Zack')
 
-movies = [movie1, movie2, movie3]
-movies.each do |movie| 
-	movie.thumbs_up
-	puts movie
-end
+playlist1.add_movie(movie1)
+playlist1.add_movie(movie2)
+playlist1.add_movie(movie3)
+
+playlist1.play
+
+playlist2 = Playlist.new('Rachel')
+playlist2.add_movie(movie3)
+
+movie4 = Movie.new('Angels in the Outfield', 7)
+playlist2.add_movie(movie4)
+
+playlist2.play
+
+
